b9a42bbead631f174f04330b314a356aecd23ca8
More2237 <noorallyrahmonov2308@gmail.com>
2024-12-06 22:36:31 +0500

    lkvjhf

---
+++ for5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("vvedite senu ");
+decimal a = decimal.Parse(Console.ReadLine());
+decimal b;
+for (decimal i = 0.1M; i<1.0M; i+=0.1M)
+{
+    b = a * i;
+    Console.WriteLine(i+" kg "  +" sena "+b);
+    
+}
